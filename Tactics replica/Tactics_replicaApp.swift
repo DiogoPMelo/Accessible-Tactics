23e2e9881f09a60f4ab58e155d0533bb023c3fe9
DiogoPMelo <diogopmelo@icloud.com>
2025-10-22 10:41:45 +0100

    Initial Commit

---
+++ Tactics replica/Tactics_replicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tactics_replicaApp.swift
+//  Tactics replica
+//
+//  Created by Diogo Melo on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Tactics_replicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tactics replicaTests/Tactics_replicaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tactics_replicaTests.swift
+//  Tactics replicaTests
+//
+//  Created by Diogo Melo on 22/10/25.
+//
+
+import Testing
+@testable import Tactics_replica
+
+struct Tactics_replicaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
